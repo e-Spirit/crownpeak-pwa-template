1e60763ec9d73f6aea218a3e5b0ba3cc5f52def8
Lukas Mauser <l.mauser@wimadev.de>
2023-02-06 09:33:40 +0100

    style(dev): change h-4/5

--- components/Dev.vue
+++ components/Dev.vue
@@ -15,7 +15,7 @@
 
       <div
         v-if="devComponentVisible"
-        class="fixed top-1/2 left-1/2 z-20 h-[80%] w-full max-w-4xl -translate-y-1/2 -translate-x-1/2 transform border shadow"
+        class="fixed top-1/2 left-1/2 z-20 h-4/5 w-full max-w-4xl -translate-y-1/2 -translate-x-1/2 transform border shadow"
       >
         <div class="bg-white p-4">
           <button @click="devComponentVisible = false">Close</button>
