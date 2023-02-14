975d3e50a0f56c1ee3a9bc7708e04f555ff16d4a
Jonas Scholz <jonas.scholz@bbscholz.de>
2023-02-14 13:12:46 +0100

    style(e2e): remove useless comments

--- cypress/e2e/locale/switch.cy.ts
+++ cypress/e2e/locale/switch.cy.ts
@@ -5,22 +5,16 @@ describe(`locale switching`, () => {
   });
 
   it("change language on /Startseite/ => redirect /Home/", () => {
-    // go to / and check if we are redirect to any other page than /
     cy.visit(`${baseURL}/Startseite/`);
-    cy.waitForHydration();
 
-    // click on the button with data-test-id="en_GB-switch"
     cy.get("button[data-testid='en_GB-switch']").click();
 
     cy.url().should("eq", `${baseURL}/Home/`);
   });
 
   it("change language, navigate back => change language back", () => {
-    // go to / and check if we are redirect to any other page than /
     cy.visit(`${baseURL}/Startseite/`);
-    cy.waitForHydration();
 
-    // click on the button with data-test-id="en_GB-switch"
     cy.get("button[data-testid='en_GB-switch']").click();
 
     cy.url().should("eq", `${baseURL}/Home/`);
@@ -28,9 +22,7 @@ describe(`locale switching`, () => {
     cy.go("back");
 
     cy.url().should("eq", `${baseURL}/Startseite/`);
-    cy.waitForHydration();
 
-    // button with data-testid de_DE-switch should be visible and have data-activeLocale="true"
     cy.get("button[data-testid='de_DE-switch']")
       .should("be.visible")
       .should("have.attr", "data-activeLocale", "true");
@@ -38,42 +30,33 @@ describe(`locale switching`, () => {
 
   it("change language => change content", () => {
     cy.visit(`${baseURL}/Startseite/`);
-    cy.waitForHydration();
 
-    // click on the button with data-test-id="en_GB-switch"
     cy.get("button[data-testid='en_GB-switch']").click();
 
     cy.url().should("eq", `${baseURL}/Home/`);
 
-    // check if the body contains welcome
     cy.get("body").should("contain", "Welcome");
   });
 
   it("change language => highlight active language", () => {
     cy.visit(`${baseURL}/Startseite/`);
-    cy.waitForHydration();
 
-    // de_DE-switch should be visible and have class bg-green-300
     cy.get("button[data-testid='de_DE-switch']")
       .should("be.visible")
       .should("have.class", "bg-green-300");
 
-    // en_GB-switch should be visible and have class bg-gray-50
     cy.get("button[data-testid='en_GB-switch']")
       .should("be.visible")
       .should("have.class", "bg-gray-50");
 
-    // click on the button with data-test-id="en_GB-switch"
     cy.get("button[data-testid='en_GB-switch']").click();
 
     cy.url().should("eq", `${baseURL}/Home/`);
 
-    // en_GB-switch should be visible and have class bg-green-300
     cy.get("button[data-testid='en_GB-switch']")
       .should("be.visible")
       .should("have.class", "bg-green-300");
 
-    // de_DE-switch should be visible and have class bg-gray-50
     cy.get("button[data-testid='de_DE-switch']")
       .should("be.visible")
       .should("have.class", "bg-gray-50");
