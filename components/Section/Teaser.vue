6c772ba7419180785aab9349f580d0830aec4e08
Lukas Mauser <l.mauser@wimadev.de>
2023-02-15 18:54:56 +0100

    style(teaser): style teaser section (#27)

--- components/Section/Teaser.vue
+++ components/Section/Teaser.vue
@@ -1,19 +1,19 @@
 <template>
   <div
-    class="grid grid-cols-2 gap-4 border bg-teal-100 p-4"
+    class="grid items-center gap-8 py-10 md:grid-cols-2"
     data-testid="teaserSection"
   >
-    <div class="col-span-2 font-bold">Teaser</div>
-
-    <div class="flex flex-col space-y-4 border p-4">
-      <h3>{{ data.st_kicker }}</h3>
-      <h2 v-if="data.st_headline">
+    <div class="flex flex-col space-y-2 p-4 md:p-0">
+      <h3 class="text-xl uppercase text-gray-800">{{ data.st_kicker }}</h3>
+      <h2 v-if="data.st_headline" class="text-3xl font-bold uppercase">
         <ElementsRichText :richtext="data.st_headline" />
       </h2>
-      <p v-if="data.st_text"><ElementsRichText :richtext="data.st_text" /></p>
+      <p v-if="data.st_text" class="text-gray-800">
+        <ElementsRichText :richtext="data.st_text" />
+      </p>
     </div>
 
-    <div v-if="data.st_picture" class="border p-4">
+    <div v-if="data.st_picture">
       <ElementsImage
         v-if="data.st_picture"
         :image="data.st_picture"
