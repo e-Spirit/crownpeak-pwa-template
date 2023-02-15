b2934cd40ed8f7628404a2e6cf786375b7ad8836
Lukas Mauser <l.mauser@wimadev.de>
2023-02-15 16:36:33 +0100

    style(slider): style slider section

--- components/Section/Header.vue
+++ components/Section/Header.vue
@@ -1,15 +1,49 @@
 <template>
-  <div class="group relative border bg-red-100 p-4" data-testid="headerSection">
+  <div class="group relative h-64" data-testid="headerSection">
     <DevOnly v-if="appDevMode || envDevMode">
       <Dev :content="data" class="hidden group-hover:block" />
     </DevOnly>
-    <span class="font-bold">Page Header</span>
+    <div
+      class="absolute inset-0 flex flex-col justify-center bg-black bg-opacity-80 p-6 text-white md:p-12"
+    >
+      <div class="max-w-xl md:border-l-8 md:p-8">
+        <h1 class="text-lg uppercase">{{ data.pt_title }}</h1>
+        <!-- <p class="text-sm text-gray-300">{{ data.pt_text }}</p> -->
+        <span
+          v-for="(breadcrumb, index) in breadcrumbs"
+          :key="'breadcrumb-' + index"
+          class="mr-2 text-xs text-gray-300"
+          >>
+          <NuxtLink
+            class="hover:underline"
+            :to="'/' + breadcrumbs.slice(0, index + 1).join('/')"
+            >{{ breadcrumb }}</NuxtLink
+          ></span
+        >
+      </div>
+    </div>
+    <ElementsImage
+      v-if="data.pt_picture"
+      class="h-full w-full object-cover"
+      :image="data.pt_picture"
+    />
   </div>
 </template>
 
 <script setup lang="ts">
-type PageHeader = {};
+import { Image } from "fsxa-api";
+
+type PageHeader = {
+  pt_picture: Image;
+  pt_text: string;
+  pt_title: string;
+};
 defineProps<{ data: PageHeader }>();
 const { devMode: appDevMode } = useAppConfig();
 const { devMode: envDevMode } = useRuntimeConfig();
+
+const breadcrumbs = computed(() => {
+  const { slug } = useRoute().params;
+  return Array.isArray(slug) ? slug.filter((el) => el) : [slug];
+});
 </script>
