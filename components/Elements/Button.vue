ff078e4fcf1c8c828978ef2a2ef1ba4ba512389d
Lukas Mauser <l.mauser@wimadev.de>
2023-02-15 18:49:25 +0100

    Style slider (#29)
    * style(slider): style slider section
    
    * fix(slider): type warning
    
    * style(button): add hover state
    
    * refactor(slider): use modolu for pagination
    
    ---------
    
    Co-authored-by: Jonas Scholz <jonas.scholz@bbscholz.de>

--- components/Elements/Button.vue
+++ components/Elements/Button.vue
@@ -1,3 +1,24 @@
 <template>
-  <div>Button Component</div>
+  <button
+    class="border border-white p-3 capitalize hover:bg-gray-800"
+    @click="clickHandler"
+  >
+    {{ button.data.lt_button_text }}
+  </button>
 </template>
+
+<script setup lang="ts">
+interface Button {
+  data: {
+    lt_button_text: string;
+    lt_product_link: { route: string };
+  };
+}
+
+const props = defineProps<{ button: Button }>();
+
+function clickHandler() {
+  const router = useRouter();
+  router.push(props.button.data.lt_product_link.route);
+}
+</script>
--- components/Section/Slider.vue
+++ components/Section/Slider.vue
@@ -1,13 +1,67 @@
 <template>
   <div data-testid="sliderSection">
-    <div class="group relative border bg-red-100 p-4">
+    <div class="group relative">
       <DevOnly v-if="appDevMode || envDevMode">
         <Dev :content="data" class="hidden group-hover:block" />
       </DevOnly>
+      <div v-if="activeSlide" class="realtive text-white">
+        <div class="sliderButtonContainer left-0">
+          <button class="sliderButton pr-8" @click="prevSlide">
+            <svg
+              xmlns="http://www.w3.org/2000/svg"
+              fill="none"
+              viewBox="0 0 24 24"
+              stroke-width="1.5"
+              stroke="currentColor"
+              class="h-6 w-6"
+            >
+              <path
+                stroke-linecap="round"
+                stroke-linejoin="round"
+                d="M15.75 19.5L8.25 12l7.5-7.5"
+              />
+            </svg>
+          </button>
+        </div>
+        <div class="sliderButtonContainer right-0">
+          <button class="sliderButton pl-8" @click="nextSlide">
+            <svg
+              xmlns="http://www.w3.org/2000/svg"
+              fill="none"
+              viewBox="0 0 24 24"
+              stroke-width="1.5"
+              stroke="currentColor"
+              class="h-6 w-6"
+            >
+              <path
+                stroke-linecap="round"
+                stroke-linejoin="round"
+                d="M8.25 4.5l7.5 7.5-7.5 7.5"
+              />
+            </svg>
+          </button>
+        </div>
+
+        <div
+          class="absolute inset-0 flex flex-col justify-end bg-gradient-to-t from-black to-transparent p-6 md:p-16"
+        >
+          <div class="flex max-w-xl flex-col space-y-4 md:py-10">
+            <h1 class="text-4xl font-bold md:text-6xl">
+              <ElementsRichText :richtext="activeSlide.data.st_title" />
+            </h1>
+            <p>{{ activeSlide.data.st_description }}</p>
+            <div><ElementsButton :button="activeSlide.data.st_button" /></div>
+          </div>
+        </div>
+
+        <ElementsImage
+          class="h-96 w-full object-cover md:h-[600px]"
+          :image="activeSlide?.data.st_picture"
+        />
+      </div>
 
       <!-- {{ activeImage }} {{ data.data }} -->
-      <!-- <pre>{{ data }}</pre> -->
-      <span class="font-bold">Slider</span>
+      <!-- <pre>{{ activeSlide.data }}</pre> -->
     </div>
   </div>
 </template>
@@ -17,7 +71,12 @@ import { Image, RichTextElement, Section } from "fsxa-api";
 
 interface SliderSlide extends Section {
   data: {
-    st_button: {};
+    st_button: {
+      data: {
+        lt_button_text: string;
+        lt_product_link: { route: string };
+      };
+    };
     st_description: string;
     st_picture: Image;
     st_picture_alt?: string;
@@ -28,5 +87,28 @@ interface SliderSlide extends Section {
 const { devMode: appDevMode } = useAppConfig();
 const { devMode: envDevMode } = useRuntimeConfig();
 
-defineProps<{ data: SliderSlide[] }>();
+const props = defineProps<{ data: SliderSlide[] }>();
+
+const activeImageIndex = ref(0);
+
+const activeSlide = computed(() => props.data[activeImageIndex.value]);
+
+function nextSlide() {
+  activeImageIndex.value = (activeImageIndex.value + 1) % props.data.length;
+}
+
+function prevSlide() {
+  activeImageIndex.value =
+    (activeImageIndex.value - 1 + props.data.length) % props.data.length;
+}
 </script>
+
+<style lang="css" scoped>
+.sliderButtonContainer {
+  @apply absolute inset-y-0 z-10 my-auto hidden items-center group-hover:flex;
+}
+
+.sliderButton {
+  @apply bg-white py-10  text-black hover:text-yellow-500;
+}
+</style>
