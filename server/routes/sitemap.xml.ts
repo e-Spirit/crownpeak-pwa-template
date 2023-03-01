21c63b7c84ea89565a7a86deea617671789da202
Lukas Mauser <l.mauser@wimadev.de>
2023-03-01 11:46:55 +0100

    feat(sitemap): add sitemap

---
+++ server/routes/sitemap.xml.ts
@@ -0,0 +1,20 @@
+import { FSXAApiSingleton } from "fsxa-api";
+
+export default defineEventHandler(async (event) => {
+  const remoteApi = FSXAApiSingleton.instance;
+  const {
+    public: { baseURL },
+  } = useRuntimeConfig();
+  const navigationData = await remoteApi.fetchNavigation({ locale: "de_DE" });
+  const seoRoutes = Object.keys(navigationData?.seoRouteMap || []);
+  const xmlString = `<?xml version="1.0" encoding="UTF-8"?>
+    <urlset>
+    ${seoRoutes
+      .map((seoRoute) => `<url><loc>${baseURL}${seoRoute}</loc></url>`)
+      .join("\n")}
+          </urlset>`;
+
+  setResponseHeader(event, "Content-Type", "text/xml");
+
+  return xmlString;
+});
