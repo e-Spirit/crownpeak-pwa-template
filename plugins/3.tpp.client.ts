da35a563ce8fb8a25adbaeb47e3e4a51ebacb45c
Jonas Scholz <jonas.scholz@bbscholz.de>
2023-03-13 11:07:00 +0100

    refactor(tpp): move hooks to utils

--- plugins/3.tpp.client.ts
+++ plugins/3.tpp.client.ts
@@ -1,6 +1,10 @@
 import { FSXAContentMode } from "fsxa-api";
 import TPP_SNAP from "fs-tpp-api";
-import { waitForPreviewId } from "~~/utils/tpp";
+import {
+  onNavigationChangeHandler,
+  onRequestPreviewElementHandler,
+  onRerenderViewHandler,
+} from "~~/utils/tpp";
 
 /**
  * Nuxt plugin that initializes the TPP Snap, only works if you are in the FirstSpirit editor and if the environment variable FSXA_MODE is set to "preview"
@@ -13,9 +17,6 @@ export default defineNuxtPlugin(() => {
   // Set by environment variable
   if (mode !== FSXAContentMode.PREVIEW) return;
 
-  const { $fsxaApi } = useNuxtApp();
-  const { activeNavigationItem } = useNavigationData();
-
   TPP_SNAP.isConnected.then(async (_isConnected: boolean) => {
     const initialized = await onInit();
 
@@ -23,47 +24,16 @@ export default defineNuxtPlugin(() => {
 
     // onRequestPreviewElement is called if you e.g. click on a result in the search results
     // https://docs.e-spirit.com/tpp/snap/#onrequestpreviewelementhandler
-    TPP_SNAP.onRequestPreviewElement(async (previewId: string) => {
-      const pageId = previewId.split(".")[0];
-      if (!pageId) return;
-
-      TPP_SNAP.setPreviewElement(previewId);
-
-      await navigateToPageId(pageId);
-    });
+    TPP_SNAP.onRequestPreviewElement(onRequestPreviewElementHandler);
 
     // onRerenderView is called if content changed and you do not have the onContentChange event handler implemented
     // The previewId can be for a dataset or a page.
     // https://docs.e-spirit.com/tpp/snap/#onrerenderviewhandler
-    TPP_SNAP.onRerenderView(async () => {
-      const previewId: string | undefined = await TPP_SNAP.getPreviewElement();
-      if (!previewId) return;
-
-      const [pageId, locale] = previewId.split(".");
-
-      if (!pageId || !locale) return;
-
-      // Wait for the CaaS to update the content
-      await waitForPreviewId($fsxaApi, previewId);
-      const { currentPage, currentDataset } = useContent();
-
-      // If our current page is a dataset, we need to fetch the dataset instead of the page
-      if (activeNavigationItem.value?.seoRouteRegex !== null) {
-        currentDataset.value = await fetchDatasetById($fsxaApi, pageId, locale);
-      } else {
-        currentPage.value = await fetchPageById($fsxaApi, pageId, locale);
-      }
-    });
+    TPP_SNAP.onRerenderView(onRerenderViewHandler);
 
     // onNavigationChange is called if anything in the navigation structure changed, e.g. a page was created or deleted
     // https://docs.e-spirit.com/tpp/snap/#onnavigationchangehandler
-    TPP_SNAP.onNavigationChange((newPagePreviewId: string | null) => {
-      // If a new page is created, onRequestPreviewElement is also triggered and will handle the navigation
-      if (newPagePreviewId) return;
-
-      // For anything else, just reloading is fine. If the page you are currently on was deleted, you will be redirected to the 404 page.
-      location.reload();
-    });
+    TPP_SNAP.onNavigationChange(onNavigationChangeHandler);
   });
 
   return {
--- snap.d.ts
+++ snap.d.ts
@@ -1,1 +1,13 @@
 declare module "fs-tpp-api";
+
+type OnRequestPreviewElementHandler = (previewId: string) => void;
+
+type OnRerenderViewHandler = () => void;
+
+type OnNavigationChangeHandler = (newPagePreviewId: string | null) => void;
+
+type OnContentChangeHandler = (
+  node: HTMLElement,
+  previewId: string,
+  content: unknown
+) => any;
