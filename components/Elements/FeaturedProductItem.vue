bdaa19c789bf1f4c2b3852f9cdafe5ed8e9d09f4
Emre Neumann <emre.neumann@crownpeak.com>
2023-05-23 14:39:05 +0200

    feat(featuredproductsection): add 2 new components: FeaturedProducts and FeaturedProductItem
    The new section can display a varying amount of product items in a responsive grid containing a
    short description and a link to the corresponding product

---
+++ components/Elements/FeaturedProductItem.vue
@@ -0,0 +1,30 @@
+<template>
+  <div
+    v-if="product.data['tt_teaser_image']"
+    class="relative z-0 w-full sm:w-[300px] lg:w-[300px] xl:w-[360px]"
+    @mouseover="hover = true"
+    @mouseleave="hover = false"
+  >
+    <img :src="imageSource" :alt="product.data.tt_name" />
+
+    <NuxtLink :to="product.route">
+      <div
+        class="absolute top-0 z-20 h-full p-4 text-white lg:p-8"
+        :class="{ [`bg-black/75`]: hover, [`bg-black/50`]: !hover }"
+      >
+        <h3 class="mt-4 border-b-2 text-2xl font-black text-yellow-500">
+          {{ product.data.tt_name }}
+        </h3>
+        <p>
+          {{ product.data.tt_abstract }}
+        </p>
+      </div>
+    </NuxtLink>
+  </div>
+</template>
+<script lang="ts" setup>
+import { DataEntry } from 'fsxa-api/dist/types'
+import { ref } from 'vue'
+defineProps<{ product: DataEntry; imageSource?: string }>()
+const hover = ref(false)
+</script>
--- components/Section/FeaturedProducts.vue
+++ components/Section/FeaturedProducts.vue
@@ -1,56 +1,63 @@
 <template>
   <div>
     <div
-      class="absolute -top-10 -z-10 hidden h-4/5 w-3/4 border-[20px] border-gray-100 md:block"
+      class="absolute -top-10 -z-10 hidden h-full w-3/4 border-[20px] border-gray-100 md:block"
     />
-    <div>
-      <h2 class="my-4 ml-8 text-4xl uppercase text-gray-400">
+    <div class="w-1/3 text-gray-400 sm:px-2 md:px-8">
+      <h2
+        class="mt-4 ml-4 text-4xl uppercase"
+        data-preview-id="#st_featured_products_title"
+      >
         {{ data.st_featured_products_title }}
       </h2>
+      <h3
+        v-if="data.st_featured_products_header"
+        class="ml-4"
+        data-preview-id="#st_featured_products_header"
+      >
+        {{ data.st_featured_products_header }}
+      </h3>
     </div>
-    <div class="grid h-4/5 grid-cols-4 gap-2 bg-red-600 md:pl-8">
+    <div
+      class="grid h-4/5 gap-2 sm:grid-cols-2 lg:grid-cols-3 lg:pl-8 2xl:grid-cols-4"
+    >
       <div
-        v-for="product in data.st_featured_products_picture"
+        v-for="(product, index) in data.st_featured_products_picture"
         :key="product.id"
       >
-        <div v-if="product.data['tt_teaser_image']" class="relative z-0">
-          <ElementsImage :image="product.data['tt_teaser_image']" />
-
-          <div class="absolute top-0 z-20 h-full bg-black/50 text-white lg:p-8">
-            <h3 class="mt-4 border-b-2 text-2xl font-black text-yellow-500">
-              {{ product.data.tt_name }}
-            </h3>
-            <p>
-              {{ product.data.tt_abstract }}
-            </p>
-            <NuxtLink :to="product.route">
-              <div
-                class="absolute bottom-8 right-8 w-20 bg-black px-4 py-2 text-center text-white"
-              >
-                More
-              </div>
-            </NuxtLink>
-          </div>
-        </div>
+        <ElementsFeaturedProductItem
+          :product="product"
+          :image-source="imgSrc[index]"
+        />
       </div>
-    </div>
-    <div class="p-2">
-      <h2
-        v-if="data.product_recommendations_subtitle"
-        class="uppercase text-gray-400"
-      >
-        {{ data.product_recommendations_subtitle }}
-      </h2>
     </div>
   </div>
 </template>
 <script setup lang="ts">
-import { DataEntries } from 'fsxa-api/dist/types'
+import { DataEntry, Image } from 'fsxa-api/dist/types'
 interface FeaturedProducts {
-  product_recommendations_subtitle?: null | string
-  st_featured_products_header: string
-  st_featured_products_picture: DataEntries
+  st_featured_products_header: null | string
+  st_featured_products_picture: DataEntry[]
   st_featured_products_title?: null | string
 }
-defineProps<{ data: FeaturedProducts }>()
+
+const props = defineProps<{ data: FeaturedProducts }>()
+const imgSrc = computed(() => {
+  const images = props.data['st_featured_products_picture'].map(
+    (product) => product.data['tt_teaser_image']
+  ) as Image[]
+  return images.map((image) => {
+    const productTeaserIndex = Object.keys(image.resolutions).findIndex(
+      (resolutionName) => resolutionName === 'product_teaser'
+    )
+    if (productTeaserIndex === -1) {
+      throw createError(
+        'The data contains no image resolution named "product_teaser". Please add one to the dataset.'
+      )
+    }
+    return Object.values(image.resolutions).map((resolution) => resolution.url)[
+      productTeaserIndex
+    ]
+  })
+})
 </script>
