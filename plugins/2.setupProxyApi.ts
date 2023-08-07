8838b8693f2991c9f1cd921e8f373d0b49a63ef4
Emre Neumann <emre.neumann@crownpeak.com>
2023-08-07 11:19:01 +0200

    fix(request-handling): split server/client urls and used 0.0.0.0 as host

--- plugins/2.setupProxyApi.ts
+++ plugins/2.setupProxyApi.ts
@@ -3,9 +3,10 @@ import { FSXAContentMode, FSXAProxyApi, LogLevel } from 'fsxa-api'
 export default defineNuxtPlugin(() => {
   const runtimeConfig = useRuntimeConfig()
   const appConfig = useAppConfig()
-  const url = new URL('/api', runtimeConfig.public['baseURL']).href
+  const clientUrl = '/api'
+  const serverUrl = 'http://0.0.0.0:3000/api'
   const fsxaApi = new FSXAProxyApi(
-    url,
+    process.client ? clientUrl : serverUrl,
     Number.parseInt(runtimeConfig.public['logLevel']) ||
       appConfig.logLevel ||
       LogLevel.NONE
