a2da275c19e0a973d5f27554d16cb9853e9df0d8
Emre Neumann <emre.neumann@crownpeak.com>
2023-08-16 13:33:34 +0200

    debug baseURL configuration with default

--- plugins/2.setupProxyApi.ts
+++ plugins/2.setupProxyApi.ts
@@ -2,9 +2,10 @@ import { FSXAContentMode, FSXAProxyApi, LogLevel } from 'fsxa-api'
 
 export default defineNuxtPlugin(() => {
   const runtimeConfig = useRuntimeConfig()
+  const baseUrl = runtimeConfig.public['baseUrl']
   const appConfig = useAppConfig()
   const clientUrl = '/api'
-  const serverUrl = 'http://localhost:3000/api'
+  const serverUrl = baseUrl ?? 'http://0.0.0.0:3000/api'
   const fsxaApi = new FSXAProxyApi(
     process.client ? clientUrl : serverUrl,
     Number.parseInt(runtimeConfig.public['logLevel']) ||
