43add8e07886f954327e8989b879f871e3583424
Emre Neumann <emre.neumann@crownpeak.com>
2023-08-16 14:51:28 +0200

    added debug logging

--- plugins/2.setupProxyApi.ts
+++ plugins/2.setupProxyApi.ts
@@ -4,11 +4,11 @@ export default defineNuxtPlugin(() => {
   const runtimeConfig = useRuntimeConfig()
   const baseUrl = runtimeConfig.public['baseUrl'] as string
   const appConfig = useAppConfig()
-  const { $logger } = useNuxtApp()
   const clientUrl = '/api'
   const serverUrl = baseUrl ?? 'http://0.0.0.0:3000/api'
   const proxyUrl = process.client ? clientUrl : serverUrl
-  $logger.debug('Proxy API initialized with URL: ', proxyUrl)
+  // eslint-disable-next-line no-console
+  console.debug('Proxy API initialized with URL: ', proxyUrl)
   const fsxaApi = new FSXAProxyApi(
     proxyUrl,
     Number.parseInt(runtimeConfig.public['logLevel']) ||
