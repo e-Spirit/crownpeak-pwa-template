ba966a50851ea5f49a92444c9190f9e1c230727c
Dean Gite <deangite@gmail.com>
2023-09-04 23:23:40 +0200

    update base url

--- plugins/2.setupProxyApi.ts
+++ plugins/2.setupProxyApi.ts
@@ -3,8 +3,11 @@ import { FSXAContentMode, FSXAProxyApi, LogLevel } from 'fsxa-api'
 export default defineNuxtPlugin(() => {
   const runtimeConfig = useRuntimeConfig()
   const appConfig = useAppConfig()
-  const clientUrl = new URL('/api', runtimeConfig.public['baseURL']).href
+  let clientUrl = '/api'
   const serverUrl = 'http://0.0.0.0:3000/api'
+  if (runtimeConfig.public['baseURL']) {
+    clientUrl = new URL('/api', runtimeConfig.public['baseURL']).href
+  }
   const fsxaApi = new FSXAProxyApi(
     process.client ? clientUrl : serverUrl,
     Number.parseInt(runtimeConfig.public['logLevel']) ||
