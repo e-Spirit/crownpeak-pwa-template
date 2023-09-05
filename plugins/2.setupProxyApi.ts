1582296f8a0f9bfa085e136c66db25f2cc18fca8
henczi <henczi@e-spirit.com>
2023-09-05 12:15:39 +0200

    chore(proxy): update serverUrl

--- plugins/2.setupProxyApi.ts
+++ plugins/2.setupProxyApi.ts
@@ -1,16 +1,20 @@
+import * as os from 'os'
 import { FSXAContentMode, FSXAProxyApi, LogLevel } from 'fsxa-api'
 
 export default defineNuxtPlugin(() => {
   const runtimeConfig = useRuntimeConfig()
   const appConfig = useAppConfig()
   const clientUrl = '/api'
-  const serverUrl = 'http://0.0.0.0:3000/api'
+  const serverUrl = 'http://127.0.0.1:3000/api'
   const fsxaApi = new FSXAProxyApi(
     process.client ? clientUrl : serverUrl,
     Number.parseInt(runtimeConfig.public['logLevel']) ||
       appConfig.logLevel ||
       LogLevel.NONE
   )
+  if (!process.client) {
+    console.log('NWInterfaces: ' + os.networkInterfaces())
+  }
   return {
     provide: {
       fsxaApi,
