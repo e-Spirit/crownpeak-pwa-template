6e9af470be7c304d7654bb5da480daf586029ce3
Dean Gite <deangite@gmail.com>
2023-09-08 15:17:39 +0200

    remove watcher

--- layouts/default.vue
+++ layouts/default.vue
@@ -18,38 +18,35 @@ const { setNavigationData, fetchNavigationData } = useNavigationData()
 const { $logger } = useNuxtApp()
 
 // This gets called when the layout is loaded or the locale changes
-const { pending } = useAsyncData(
-  async () => {
-    // fetch project properties
-    const projectProperties = await fetchProjectProperties(activeLocale.value!)
-    if (!projectProperties) {
-      $logger.error(
-        'Project properties could not be fetched for locale: ',
-        activeLocale.value
-      )
-      throw showError({
-        message: 'Project properties could not be fetched',
-        statusCode: 500
-      })
-    }
+const { pending } = useAsyncData(async () => {
+  // fetch project properties
+  const projectProperties = await fetchProjectProperties(activeLocale.value!)
+  if (!projectProperties) {
+    $logger.error(
+      'Project properties could not be fetched for locale: ',
+      activeLocale.value
+    )
+    throw showError({
+      message: 'Project properties could not be fetched',
+      statusCode: 500
+    })
+  }
 
-    setProjectProperties(projectProperties, activeLocale.value!)
+  setProjectProperties(projectProperties, activeLocale.value!)
 
-    // fetch navigationData
-    const navigationData = await fetchNavigationData(activeLocale.value!)
-    if (!navigationData) {
-      $logger.error(
-        'Navigation data could not be fetched for locale: ',
-        activeLocale.value
-      )
-      throw showError({
-        message: 'Navigation data could not be fetched',
-        statusCode: 500
-      })
-    }
+  // fetch navigationData
+  const navigationData = await fetchNavigationData(activeLocale.value!)
+  if (!navigationData) {
+    $logger.error(
+      'Navigation data could not be fetched for locale: ',
+      activeLocale.value
+    )
+    throw showError({
+      message: 'Navigation data could not be fetched',
+      statusCode: 500
+    })
+  }
 
-    setNavigationData(navigationData)
-  },
-  { watch: [activeLocale] }
-)
+  setNavigationData(navigationData)
+})
 </script>
