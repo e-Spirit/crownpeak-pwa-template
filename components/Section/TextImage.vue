312455f724984deb5ba52d07658a901907e21994
Emre Neumann <emre.neumann@crownpeak.com>
2023-11-13 14:14:33 +0100

    added text-image component

--- components/Section/TextImage.vue
+++ components/Section/TextImage.vue
@@ -1,7 +1,23 @@
 <template>
-  <div>Text IMAGEEEEE</div>
+  <div>
+    <h2>{{ data.st_headline }}</h2>
+    <ElementsRichText :richtext="data.st_subheadline" />
+    <ElementsImage :image="data.st_image" :alt="data.st_image_alt_text" />
+    <ElementsRichText :richtext="data.st_text" />
+  </div>
 </template>
 
-<script setup lang="ts"></script>
+<script setup lang="ts">
+import { RichTextElement, Image } from 'fsxa-api'
+
+interface TextImage {
+  st_headline: string
+  st_subheadline: RichTextElement[]
+  st_text: RichTextElement[]
+  st_image: Image
+  st_image_alt_text?: string
+}
+defineProps<{ data: TextImage }>()
+</script>
 
 <style scoped></style>
