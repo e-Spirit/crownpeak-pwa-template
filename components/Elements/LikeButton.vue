f8228598e079579cf5598e81311ddd340771d0fa
Emre Neumann <emre.neumann@crownpeak.com>
2023-11-17 16:56:38 +0100

    changed product detail view

---
+++ components/Elements/LikeButton.vue
@@ -0,0 +1,36 @@
+<template>
+  <button
+    @click="toggleState"
+    class="ml-auto mr-4 inline-flex h-16 w-16 flex-shrink-0 items-center justify-center rounded-md border hover:border-gray-500 sm:ml-0"
+  >
+    <svg
+      id="heart25"
+      class="fav_heart h-6 w-6"
+      width="27"
+      height="27"
+      viewBox="0 0 27 27"
+      :fill="fill"
+      xmlns="http://www.w3.org/2000/svg"
+    >
+      <path
+        d="M13.4993 26.2061L4.70067 16.9253C3.9281 16.1443 3.41815 15.1374 3.24307 14.0471C3.06798 12.9568 3.23664 11.8385 3.72514 10.8505V10.8505C4.09415 10.1046 4.63318 9.45803 5.29779 8.96406C5.96241 8.47008 6.73359 8.14284 7.54782 8.00931C8.36204 7.87578 9.19599 7.93978 9.98095 8.19603C10.7659 8.45228 11.4794 8.89345 12.0627 9.48319L13.4993 10.9358L14.9359 9.48319C15.5192 8.89345 16.2327 8.45228 17.0177 8.19603C17.8026 7.93978 18.6366 7.87578 19.4508 8.00931C20.265 8.14284 21.0362 8.47008 21.7008 8.96406C22.3654 9.45803 22.9045 10.1046 23.2735 10.8505V10.8505C23.762 11.8385 23.9306 12.9568 23.7556 14.0471C23.5805 15.1374 23.0705 16.1443 22.298 16.9253L13.4993 26.2061Z"
+        stroke="black"
+        stroke-width="1.5"
+        stroke-linecap="round"
+        stroke-linejoin="round"
+      ></path>
+    </svg>
+  </button>
+</template>
+
+<script setup lang="ts">
+const active = ref(false)
+const toggleState = () => {
+  active.value = !active.value
+}
+const fill = computed(() => {
+  return active.value ? 'black' : 'none'
+})
+</script>
+
+<style scoped></style>
--- components/Section/Product.vue
+++ components/Section/Product.vue
@@ -1,5 +1,41 @@
 <template>
-  <div
+  <section v-if="currentDataset" class="overflow-x-hidden py-12 md:py-32">
+    <div class="container mx-auto px-4">
+      <div class="flex flex-wrap lg:flex-nowrap">
+        <div class="w-full lg:w-1/2">
+          <div class="py-6 lg:pr-32">
+            <div class="mb-4">
+              <span
+                v-for="category in categoryNames"
+                :key="category"
+                class="rounded-xl bg-gray-900 px-3 py-1 text-xs font-semibold text-white"
+                >{{ category }}</span
+              >
+              <h1>{{ currentDataset.data['tt_name'] }}</h1>
+            </div>
+            <div class="flex items-start py-4">
+              <ElementsRichText
+                :richtext="currentDataset.data['tt_description']"
+              />
+            </div>
+            <div class="w-full px-4">
+              <ElementsLikeButton />
+            </div>
+            <div
+              class="mt-10 flex items-start border-t border-gray-200 py-4 pt-10"
+            >
+              <p class="text-lg text-gray-900 sm:text-3xl">
+                {{ currentDataset.data['tt_price'] }}
+              </p>
+            </div>
+            <div class="relative my-12 w-full lg:my-0 lg:w-1/2">
+              <ElementsImage :image="currentDataset.data['tt_image']" />
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  </section>
     class="grid lg:grid-cols-2 lg:gap-4 lg:pt-4"
     data-testid="productSection"
   >
@@ -161,15 +197,17 @@
         </div>
       </div>
     </div>
-  </div>
+  </div> -->
 </template>
 
 <script setup lang="ts">
-import { DataEntries } from 'fsxa-api'
+import { DataEntries, Dataset } from 'fsxa-api'
 defineProps<{ data: DataEntries }>()
 
 const { currentDataset } = useContent()
-
-const deliveryExpanded = ref(false)
-const installationExpanded = ref(false)
+const categoryNames = computed(() => {
+  return currentDataset.value?.data['tt_categories'].flatMap(
+    (category: Dataset) => category.data['tt_name']
+  )
+})
 </script>
