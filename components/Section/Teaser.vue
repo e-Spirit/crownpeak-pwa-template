9cd130e37932aab71e94161ee53d4d9649608951
Emre Neumann <emre.neumann@crownpeak.com>
2023-11-30 18:07:47 +0100

    fixed teaser when alternative layout was selected

--- components/Section/Teaser.vue
+++ components/Section/Teaser.vue
@@ -33,16 +33,29 @@
         v-if="data.st_layout.key === 'image-text'"
         class="-mx-4 flex flex-wrap items-center"
       >
-        <ElementsTeaserImage
-          :image="data.st_image"
-          :alt-text="data.st_image_alt_text"
-        />
-        <ElementsTeaserText :headline="data.st_headline" :text="data.st_text" />
-        <ElementsInternalLink v-if="data.st_cta" :link-data="data.st_cta">
-          <button class="btn btn-primary">
-            {{ data.st_cta.data.lt_text }}
-          </button>
-        </ElementsInternalLink>
+        <div class="px-4 sm:w-full md:w-1/2">
+          <ElementsTeaserImage
+            :image="data.st_image"
+            :alt-text="data.st_image_alt_text"
+          />
+        </div>
+        <div class="mt-8 px-4 text-left sm:w-full md:w-1/2">
+          <ElementsTeaserText
+            :headline="data.st_headline"
+            :text="data.st_text"
+          />
+          <div class="flex flex-wrap">
+            <ElementsInternalLink v-if="data.st_cta" :link-data="data.st_cta">
+              <div class="w-full py-1 md:mr-4 md:w-auto md:py-0">
+                <span
+                  class="inline-block w-full rounded-md bg-secondary px-7 py-5 text-center text-base font-medium leading-4 text-white hover:brightness-90 md:text-lg"
+                >
+                  {{ data.st_cta.data.lt_text }}
+                </span>
+              </div>
+            </ElementsInternalLink>
+          </div>
+        </div>
       </div>
     </div>
   </section>
