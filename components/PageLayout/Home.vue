5eba51fd7790119391b54c599f2ebc6b29c9a779
nico-mcalley <nicolai.mcalley@crownpeak.com>
2024-01-17 11:32:09 +0100

    fix(PageLayout): Home layout shows content changes (#132)
    * fix(PageLayout): iterate over bodies in reverse order
    * fix(tests): fix e2e tests sitemap fixture

--- components/PageLayout/Home.vue
+++ components/PageLayout/Home.vue
@@ -1,15 +1,14 @@
 <template>
   <div data-testid="homePageLayout">
-    <PageBody :page-body="top" />
-    <PageBody :page-body="content" />
+    <PageBody
+      v-for="pageBody in page.children.slice().reverse()"
+      :key="pageBody.previewId"
+      :page-body="pageBody"
+    />
   </div>
 </template>
 
 <script setup lang="ts">
 import { Page } from 'fsxa-api'
-const props = defineProps<{ page: Page }>()
-const top = props.page.children.find((pagebody) => pagebody.name === 'top')!
-const content = props.page.children.find(
-  (pagebody) => pagebody.name === 'content'
-)!
+defineProps<{ page: Page }>()
 </script>
