f1683e28e2f8da855e38066df6b86b12597e79f0
Nicolai McAlley <nicolai.mcalley@crownpeak.com>
2024-01-22 13:38:09 +0100

    fix(Home): add declarative order for PageBodies

--- components/PageLayout/Home.vue
+++ components/PageLayout/Home.vue
@@ -1,14 +1,19 @@
 <template>
   <div data-testid="homePageLayout">
-    <PageBody
-      v-for="pageBody in page.children.slice().reverse()"
-      :key="pageBody.previewId"
-      :page-body="pageBody"
-    />
+    <PageBody :page-body="top" />
+    <PageBody :page-body="content" />
   </div>
 </template>
 
 <script setup lang="ts">
 import { Page } from 'fsxa-api'
-defineProps<{ page: Page }>()
+import { computed } from 'vue'
+
+const props = defineProps<{ page: Page }>()
+const top = computed(() => {
+  return props.page.children.find((pagebody) => pagebody.name === 'top')!
+})
+const content = computed(() => {
+  return props.page.children.find((pagebody) => pagebody.name === 'content')!
+})
 </script>
